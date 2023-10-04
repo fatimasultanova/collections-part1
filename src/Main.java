035cd8b3530732cd60c838b49dc9781b93aed2a2
Fatima <fatimasultanova.909@gmail.com>
2023-10-04 21:46:01 +0400

    MyLinkedList added

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,36 @@
 import lists.MyArrayList;
-
+import lists.MyLinkedList;
 public class Main {
     public static void main(String[] args) {
-        MyArrayList<String> myArrayList = new MyArrayList<>();
-        myArrayList.add("a");
-        myArrayList.add("b");
-        myArrayList.add("c");
-        myArrayList.add("d");
-        myArrayList.add("e");
-        System.out.println(myArrayList);
-        System.out.println("------------------------");
-        System.out.println(myArrayList.get(2));
-        myArrayList.remove(4);
-        System.out.println(myArrayList);
-        System.out.println("------------------------");
-        myArrayList.add(4,"cc");
-        System.out.println(myArrayList);
+        MyLinkedList<String> myLinkedList= new MyLinkedList<>();
+        myLinkedList.add("a");
+        myLinkedList.add("b");
+        myLinkedList.add("c");
+        myLinkedList.add("d");
+        System.out.println(myLinkedList);
+        myLinkedList.addFirst("aa");
+        System.out.println(myLinkedList);
+        myLinkedList.peek();
+        System.out.println(myLinkedList);
+        myLinkedList.poll();
+        System.out.println(myLinkedList);
 
+//        MyArrayList<String> myArrayList = new MyArrayList<>();
+//        myArrayList.add("a");
+//        myArrayList.add("b");
+//        myArrayList.add("c");
+//        myArrayList.add("d");
+//        myArrayList.add("e");
+//        System.out.println(myArrayList);
+//        System.out.println("------------------------");
+//        System.out.println(myArrayList.get(2));
+//        myArrayList.remove(4);
+//        System.out.println(myArrayList);
+//        System.out.println("------------------------");
+//        myArrayList.add(4,"cc");
+//        System.out.println(myArrayList);
 
+//        LinkedList<String> linkedList = new LinkedList<>();
+//        linkedList.poll()
     }
 }
--- src/lists/MyLinkedList.java
+++ src/lists/MyLinkedList.java
@@ -1,4 +1,91 @@
 package lists;
 
-public class MyLinkedList {
+import java.util.LinkedList;
+
+public class MyLinkedList<E> {
+    private Node<E> head;
+    private int size;
+
+    private void setHead(Node<E> head) {
+        this.head = head;
+    }
+
+    static class Node<E> {
+        private E element;
+        private Node<E> next;
+
+        public Node(E element) {
+            this.element = element;
+        }
+
+        public void setNext(Node<E> next) {
+            this.next = next;
+        }
+
+
+    }
+
+    public MyLinkedList() {
+
+
+    }
+
+    public void add(E element) {
+        Node<E> node = new Node<>(element);
+
+        if (head == null) {
+            this.head = node;
+        } else {
+            Node<E> currentNode = head;
+            while (currentNode.next != null) {
+                currentNode = currentNode.next;
+            }
+
+            currentNode.setNext(node);
+        }
+        size++;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("[");
+        Node<E> currentNode = this.head;
+        while (currentNode != null) {
+            stringBuilder.append(currentNode.element);
+            if ((currentNode = currentNode.next) != null) {
+                stringBuilder.append(", ");
+            }
+        }
+        stringBuilder.append("]");
+        return stringBuilder.toString();
+
+    }
+
+    private void checkIndex(int index) {
+        if (index >= size || index < 0) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+    }
+
+    public void addFirst(E element) {
+        Node<E> node = new Node<>(element);
+       setHead(node);
+    }
+
+//    public E get(int index){
+//        checkIndex(index);
+//        }
+
+    public E peek() {
+        return head.element;
+    }
+
+    public E poll(){
+        E element = head.element;
+        head.element = null;
+        size--;
+        return element;
+    }
+
 }
